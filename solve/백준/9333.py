e25fd2cbcdb442e287869395384d006978bc1267
oxhwixo <hwi_030@naver.com>
2023-03-19 23:11:57 +0900

    230319

--- solve/백준/9333.py
+++ solve/백준/9333.py
@@ -6,6 +6,26 @@ input = sys.stdin.readline
 
 n = int(input())
 
+for _ in range(n):
+  r, b, m = map(float, input().split())
+
+  count = 0
+  
+  while b > 0:
+    count += 1
+    if count > 1200:
+      count = -1
+      break
+
+    b += (b * r) / 100
+    b = round(b, 8)
+    if (b * 1000) % 10 == 5:
+      b += 0.001
+    b = round(b, 2)
+    b -= m
+
+  print("impossible" if count == -1 else count)
+
 # def my_round(number):
 #   number = round(round(number, 8), 3)
 
